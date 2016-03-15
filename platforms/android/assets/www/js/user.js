5412d5692218b9bcd81ba43725c7d7c86aa6c7c7
Jonatán López <jonatan_angelo@hotmail.com>
2016-03-15 18:11:42 -0500

    10

--- platforms/android/assets/www/js/user.js
+++
@@ -1,8 +0,0 @@
-var User = function(info){
-	this.dni = info.dni;
-	this.nombres = info.nombres;
-	this.apellido_paterno = info.apellido_paterno;
-	this.apellido_materno = info.apellido_materno;
-
-	this.nivel = info.level;
-}
